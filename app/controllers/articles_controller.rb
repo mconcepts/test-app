807b84cdbeb0986407132ea27396b61c678b938b
mconcepts <azharmconcepts@gmail.com>
2018-03-25 09:47:12 -0400

    edit update show index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
       flash[:notice] = "article .....  Successfully created..."
       redirect_to article_path(@article)
     else
-      render 'new'
+      render :new
     end
   end
 
@@ -23,6 +23,26 @@ def show
 
 @article =Article.find(params[:id])
 
+end
+
+def edit
+
+  @article = Article.find(params[:id])
+
+end
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(article_params)
+    flash[:notice] = "article .....  Successfully updated..."
+    redirect_to article_path(@article)
+  else
+    render 'edit'
+  end
+end
+
+def index
+  @articles=Article.all
 end
 
 private
